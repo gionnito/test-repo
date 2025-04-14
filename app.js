ae224bfffeff74484701d32c19214c082a0c0a01
gionnito <giopp1996@gmail.com>
2025-04-13 19:59:25 -0500

    refactor: simplify cat face button logic on the main page

--- app.js
+++ app.js
@@ -2,9 +2,5 @@ const button = document.querySelector('.container-button')
 const text = document.querySelector('.container-text')
 
 button.addEventListener('click', () => {
-  if (text.innerText === '😸') {
-    text.innerText = '😿'
-  } else {
-    text.innerText = '😸'
-  }
+  text.innerText === '😸' ? text.innerText = '😿' : text.innerText = '😸'
 })
