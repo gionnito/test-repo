0d9ef8223fe5394d98eaf57e9dc9f83ca000a11c
gionnito <giopp1996@gmail.com>
2025-04-13 19:32:47 -0500

    feat: add cat face functionality to the main page

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-let x = 33
+const button = document.querySelector('.container-button')
+const text = document.querySelector('.container-text')
+
+button.addEventListener('click', () => {
+  if (text.innerText === '😸') {
+    text.innerText = '😿'
+  } else {
+    text.innerText = '😸'
+  }
+})
